5a4ca1e8aac34dadbfef92d4fc0162cf3df2a4b2
changwon <ckddnjs554@naver.com>
2021-03-29 01:27:23 +0900

    Feat : Kotlin 타입변환 및 null 값을 가질 수 있는 타입 선언 공부

--- app/src/main/java/com/example/kotlinstudy/Kotlin/03.Pratice.kt
+++ app/src/main/java/com/example/kotlinstudy/Kotlin/03.Pratice.kt
@@ -4,3 +4,24 @@ var a =  1 + 2 + 3 + 4 + 5
 var b = "1"
 var c = b.toInt()
 var d = b.toFloat()
+
+var e = "Jhon"
+
+// $ 는 변수 연결
+var f = "My name is $e Nice to meet yot"
+
+// null
+
+// var abc : Int = null
+// 위에 같이 선언하면 null 을 가질 수 없는 형태이고 ? <- 을 붙여주면 null 을 가질 수 있는 타입선언 이다
+var abc1 : Int? = null
+var abc2 : Double? = null
+
+fun main(array: Array<String>) {
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(f)
+    println(abc1)
+}
