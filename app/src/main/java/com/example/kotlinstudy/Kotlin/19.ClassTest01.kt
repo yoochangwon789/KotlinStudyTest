ef14bcc96ffbf24418e28c4b0dd5b5d5865fe785
changwon <ckddnjs554@naver.com>
2021-04-08 16:06:59 +0900

    Feat : Kotlin class 사칙연산 객체 만들기

--- app/src/main/java/com/example/kotlinstudy/Kotlin/19.ClassTest01.kt
+++ app/src/main/java/com/example/kotlinstudy/Kotlin/19.ClassTest01.kt
@@ -2,6 +2,40 @@ package com.example.kotlinstudy.Kotlin
 
 fun main(array: Array<String>) {
 
+    val calculator: Calculator = Calculator(10, 20)
+    println("두개의 숫자를 더한 결과 : ${calculator.sum()}")
+    println("두개의 숫자를 뺀 결과 : ${calculator.minus()}")
+    println("두개의 숫자를 곱한 결과 : ${calculator.multiplication()}")
+    println("두개의 숫자를 나눈 결과 : ${calculator.division()}")
+    println("두개의 숫자를 나눈 나머지 결과 : ${calculator.remainder()}")
 }
 
-class Calculator
+class Calculator(var number1: Int, var number2: Int) {
+
+    var result: Int = 0
+
+    fun sum(): Int {
+        result = number1 + number2
+        return result
+    }
+
+    fun minus(): Int {
+        result = number1 - number2
+        return result
+    }
+
+    fun multiplication(): Int {
+        result = number1 * number2
+        return result
+    }
+
+    fun division(): Int {
+        result = number1 / number2
+        return result
+    }
+
+    fun remainder(): Int {
+        result = number1 % number2
+        return result
+    }
+}
