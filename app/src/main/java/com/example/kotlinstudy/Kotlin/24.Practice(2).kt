2c1a685c916e81d0eac1d224bc719b5d444e9e3d
CN <ckddnjs554@naver.com>
2021-04-11 21:26:12 +0900

    Feat : Kotlin class 상속 코딩 연습 풀이

--- app/src/main/java/com/example/kotlinstudy/Kotlin/24.Practice(2).kt
+++ app/src/main/java/com/example/kotlinstudy/Kotlin/24.Practice(2).kt
@@ -6,28 +6,41 @@ fun main(args: Array<String>) {
     val night = SuperNight(130, 8)
 
     monster.attack(night, 10)
+    monster.bite(night)
 }
+
+// 상속이 만들어낸 특징
+// - 자식 클래스는 부모 클래스의 타입이다
+// - 부모 클래스는 자식 클래스의 타입이 아니다
 
 open class Charator(var hp: Int, val power: Int) {
 
-    fun attack(charator: Charator, power: Int) {
+    open fun attack(charator: Charator, power: Int = this.power) {
         charator.defense(power)
     }
 
-    fun defense(damage: Int) {
+    open fun defense(damage: Int) {
         hp -= damage
 
-        if (hp > 0) println("남은 체력 $hp")
+        // 클래스의 이름 불러오는 javaClass 를 활용해 그 클래스의 이름을 가져온다
+        if (hp > 0) println("${javaClass.simpleName}의 남은 체력은 $hp 입니다.")
         else println("사망했습니다")
     }
 }
 
 // 자식 클래스가 인스턴스화 되기 위해서 부모 클래스가 선행되서 인스턴스화 되어야 한다
 // 자식의 멤버변수들은 val, var 을 붙일 수 없다
-class SuperMonster(hp: Int, power: Int): Charator(hp, power) {
+class SuperMonster(hp: Int, power: Int) : Charator(hp, power) {
 
+    fun bite(charator: Charator) {
+        super.attack(charator, power + 2)
+    }
 }
 
-class SuperNight(hp: Int, power: Int): Charator(hp, power) {
+class SuperNight(hp: Int, power: Int) : Charator(hp, power) {
+    val defensePower = 2
 
+    override fun defense(damage: Int) {
+        super.defense(damage - defensePower)
+    }
 }
