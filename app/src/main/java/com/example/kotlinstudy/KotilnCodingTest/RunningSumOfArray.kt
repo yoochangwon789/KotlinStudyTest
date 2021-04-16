13f93bd8b7bc5cfd7f435ddeddbd7b1e350fd1c2
CN <ckddnjs554@naver.com>
2021-04-16 10:51:39 +0900

    Feat : Kotlin LeetCode RunningSumOfArray 문제 해결 중

---
+++ app/src/main/java/com/example/kotlinstudy/KotilnCodingTest/RunningSumOfArray.kt
@@ -0,0 +1,21 @@
+package com.example.kotlinstudy.KotilnCodingTest
+
+fun main(array: Array<String>) {
+
+}
+
+fun runningSum(nums: IntArray): IntArray {
+    var answer: IntArray = intArrayOf()
+
+    var top: Int = 1
+
+    while (top == nums.size) {
+
+        for (i in 0 until top) {
+
+        }
+    }
+
+
+    return answer
+}
