08a020478adb6779f7fea1be67046a7a18370130
CN <ckddnjs554@naver.com>
2021-05-16 21:24:08 +0900

    Feat : Kotlin 프로그래머스 Level1 로또최고순위와최저순위

---
+++ app/src/main/java/com/example/kotlinstudy/Programmers/Level1.로또최고순위와최저순위.kt
@@ -0,0 +1,70 @@
+package com.example.kotlinstudy.Programmers
+
+fun main(array: Array<String>) {
+
+    val a = intArrayOf(1,6,10,19,31,45)
+    val b = intArrayOf(0,0,0,0,0,0)
+
+    val lotto = Lotto()
+
+    print(lotto.solution(b, a))
+}
+
+class Lotto() {
+    fun solution(lottos: IntArray, win_nums: IntArray): MutableList<Int> {
+        var answer = mutableListOf<Int>()
+
+        var lottosCheck: Int = 0
+        var zeroCount: Int = 0
+        var win: Int = 0
+
+        var zeroCheckArray = mutableListOf<Int>()
+
+        var sotredWinNums = win_nums.sorted()
+
+        for (i in lottos.indices)  {
+            if (lottos[i] != 0){
+                zeroCheckArray.add(lottos[i])
+            } else zeroCount++
+        }
+
+        println(zeroCheckArray)
+        println(zeroCount)
+
+        if (zeroCheckArray.size > 0) {
+            for (i in zeroCheckArray.indices) {
+                for (j in win_nums.indices) {
+                    if (zeroCheckArray[i] == win_nums[j]) lottosCheck++
+                }
+            }
+        }
+
+        println(lottosCheck)
+
+        // 최고순위
+        if (zeroCount >= 0) {
+            win = lottosCheck + zeroCount
+
+            when (win) {
+                6 -> answer.add(1)
+                5 -> answer.add(2)
+                4 -> answer.add(3)
+                3 -> answer.add(4)
+                2 -> answer.add(5)
+                else -> answer.add(6)
+            }
+        }
+
+        // 최저순위
+        when (lottosCheck) {
+            6 -> answer.add(1)
+            5 -> answer.add(2)
+            4 -> answer.add(3)
+            3 -> answer.add(4)
+            2 -> answer.add(5)
+            else -> answer.add(6)
+        }
+
+        return answer
+    }
+}
