6a32a8136d672dd2a1565d9cc4c313a749d5f96b
CN <ckddnjs554@naver.com>
2021-05-23 04:42:44 +0900

    Feat : Kotlin 프로그래머스 Level2 기능개발 문제 해결 중

---
+++ app/src/main/java/com/example/kotlinstudy/Programmers/Level2.기능개발.kt
@@ -0,0 +1,35 @@
+package com.example.kotlinstudy.Programmers
+
+fun main(array: Array<String>) {
+
+    val aArray = intArrayOf(93, 30, 55)
+    val bArray = intArrayOf(1, 30, 5)
+
+    val functionDevelopment = FunctionDevelopment()
+    println(functionDevelopment.solution(aArray, bArray))
+}
+
+class FunctionDevelopment() {
+    fun solution(progresses: IntArray, speeds: IntArray): MutableList<Int> {
+        var answer = mutableListOf<Int>()
+        var top: Int = 1
+        var check: Int = 0
+
+        val checkArray = mutableListOf<Int>()
+
+        for (i in progresses.indices) {
+            if ((100 - progresses[i]) % speeds[i] >= 1) {
+                check = (100 - progresses[i]) / speeds[i] + 1
+                checkArray.add(check)
+            } else {
+                check = (100 - progresses[i]) / speeds[i]
+                checkArray.add(check)
+            }
+            check = 0
+        }
+
+        // 7, 3, 9
+
+        return answer
+    }
+}
